7064599b35969b70bb779fd63c826d179116b212
Felipe Reigosa <felipereigosa@gmail.com>
2022-06-20 17:02:04 -0300

    Removed cannon-es javascript file

--- resources/public/libraries.js
+++ resources/public/libraries.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'GLTFLoader';
 import {VRButton} from 'VRButton';
 import {OrbitControls} from 'OrbitControls';
-import * as CANNON from './cannon-es.js'
+import * as CANNON from 'cannon'
 
 window.THREE = THREE;
 window.VRButton = VRButton;
